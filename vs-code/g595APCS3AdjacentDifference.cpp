68f7df0ef47aaa82c0074842b20108413809d751
penguin_72487 <agar.io6100@gmail.com>
2021-11-08 15:52:13 +0800

    差分

--- vs-code/g595APCS3AdjacentDifference.cpp
+++ vs-code/g595APCS3AdjacentDifference.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<algorithm>
+#include<queue>
 using namespace std;
 bool cmp(int a,int b)
 {
@@ -10,18 +11,23 @@ int main()
     cin.tie(0)->sync_with_stdio(0);
     int n, k;
     cin >> n >> k;
-    int cost[n];
-    fill(cost,cost+n,0);
+    int Cost[n];
+    fill(Cost,Cost+n,0);
     for (int i = 0; i < k;++i)
     {
         int a, b,h;
         cin >> a >> b>>h;
-        for (int i = a - 1; i < b;++i)
-        {
-            cost[i] += h;
-        }
+        Cost[a-1] += h;
+        Cost[b] -= h;
     }
-    sort(cost,cost+n,cmp);
+    //int cost[n];
+    for (int i_Sum=0,i = 0; i < n;++i)
+    {
+        i_Sum += Cost[i];
+        Cost[i] = i_Sum;
+        
+    }
+    sort(Cost, Cost + n, cmp);
     int list[n];
     
     long long ans = 0;
@@ -33,7 +39,7 @@ int main()
     sort(list, list + n);
     for (int i = 0; i < n;++i)
     {
-        ans+=list[i]*cost[i];
+        ans+=list[i]*Cost[i];
     }
     cout << ans << "\n";
 }

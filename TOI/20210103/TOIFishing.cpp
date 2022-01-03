ae5e617deb33b817f1dda6161f3bcdd69e3d73b7
penguin_72487 <agar.io6100@gmail.com>
2022-01-03 18:50:13 +0800

    TOI

---
+++ TOI/20210103/TOIFishing.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+int qury(int n, int *f, int init);
+int main()
+{
+    int n;
+    cin >> n;
+    int f[n];
+    for (int i = 0; i < n;++i)
+    {
+        cin >> f[i];
+    }
+    int q;
+    cin >> q;
+    for (int i = 0; i < n;++i)
+    {
+        int k;
+        cin >> k;
+        int op = 1,ed=k;
+        int mid;
+        while(op<ed)
+        {
+            mid = (op + ed) / 2;
+            //cout << mid << " ";
+            if(qury(n,f,mid)<k)
+            {
+                op = mid+1;
+            }
+            else
+            {
+                ed = mid-1;
+            }
+        }
+        cout << op <<"\n";
+    }
+}
+
+int qury(int n, int *f, int init)
+{
+    int ans = init;
+    int tmp = init;
+    for (int i = 0; i < n;++i)
+    {
+        tmp = tmp * ( min(log2(f[i] + 1), 30.0) / 30);
+        ans += tmp;
+    }
+
+        return ans;
+}

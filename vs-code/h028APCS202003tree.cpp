ac59016f0f7f1fe781af85689f62fa7d2864e9eb
penguin_72487 <agar.io6100@gmail.com>
2022-03-04 10:58:33 +0800

    複數

--- vs-code/h028APCS202003tree.cpp
+++ vs-code/h028APCS202003tree.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
-#include<list>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <queue>
 using namespace std;
 int main()
 {
@@ -23,9 +24,21 @@ int main()
     {
         seg.push_back(fk_tree_Loc[i+1]-fk_tree_Loc[i]);
     }
+    seg.push_back(ed-fk_tree_Loc[n]);
     for (auto it = seg.begin(); it != seg.end();++it)
     {
         cout << *it << " ";
     }
     cout << "\n";
+    int ans = 0;
+    queue<list<int>::iterator> STL;
+    auto it = seg.begin();
+    for (int i=0; i < n;++i)
+    {
+        if(tree_H[i]<*it||tree_H[i]<*next(it))
+        {
+            STL.push();
+            break;
+        }
+    }
 }

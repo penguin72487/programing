39a8489db28df7e858f1e7ed3d88665eec1b8a75
penguin_72487 <agar.io6100@gmail.com>
2022-03-16 21:34:25 +0800

    remove q_sqrt

--- essay/path_Astar/Astart_algorithm.cpp
+++ essay/path_Astar/Astart_algorithm.cpp
@@ -4,17 +4,6 @@
 #include<vector>
 using namespace std;
 ofstream path("maze0Path.txt");
-float q_sqrt(unsigned long long x) { 
-	if(x == 0) return 0; 
-	float result = x; 
-	float xhalf = 0.5f*result; 
-	int i = *(int*)&result; 
-	i = 0x5f375a86- (i>>1); // what the fuck? 
-	result = *(float*)&i; 
-	result = result*(1.5f-xhalf*result*result); // Newton step, repeating increases accuracy 
-	result = result*(1.5f-xhalf*result*result); 
-	return 1.0f/result; 
-}
 class node{
 public:
     int x, y;

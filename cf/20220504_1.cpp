463e150d034f182dd02cc60d839fa7a3b076fac2
penguin_72487 <agar.io6100@gmail.com>
2022-05-21 14:56:41 +0800

    ??

---
+++ cf/20220504_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        int s;
+        cin >> n >> s;
+        int ans=0;
+        for(int i=0; i<n;++i)
+        {
+            int tmp;
+            cin >> tmp;
+            ans += tmp;
+        }
+        if(ans==s)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+    }
+}

8f11b620ea5cf3252602a84389ed46f4a4d0d8ab
penguin_72487 <agar.io6100@gmail.com>
2022-09-24 21:34:17 +0800

    task1673 debug

--- graph/task1673Bellman-Ford.cpp
+++ graph/task1673Bellman-Ford.cpp
@@ -2,19 +2,51 @@
 #include<vector>
 using namespace std;
 vector<long long> dist;
-long long bellman_Ford(int now)
+vector<vector<pair<int, int>>> tunnel;
+vector<bool> pass_By;
+bool flag=1;
+void bellman_Ford(int now)
 {
-
+    for(auto it=tunnel[now].begin();it!=tunnel[now].end();++it)
+    {
+        if(pass_By[it->first])
+        {
+            continue;
+        }
+        if(it->second+dist[now]>dist[it->first])
+        {
+            flag=1;
+            dist[it->first] = it->second + dist[now];
+            bellman_Ford(it->first);
+        }
+    }
 }
+long long bellman_Ford(int now,int target)
+{
+    pass_By.resize(dist.size());
+    for(long long i=0;i<dist.size()&&flag;++i)
+    {
+        flag = 0;
+        fill(pass_By.begin(),pass_By.end(),0);
+        pass_By[now] = 1;
+        bellman_Ford(now);
+    }
+    if(flag)
+    {
+        return -1ll;
+    }
+    return dist[target];
+}
+
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     cout.tie(0);
     int n, m;
     cin >> n >> m;
-    vector<vector<pair<int, int>>> tunnel(n); // adj list
+    tunnel.resize(n); // adj list
     
 
-    for(int i=0,a,b,c; i<n; ++i)
+    for(int i=0,a,b,c; i<m; ++i)
     {
         cin >> a >> b>>c;
         --a;
@@ -22,9 +54,9 @@ int main(){
         tunnel[a].push_back(make_pair(b,c));
     }
     dist.resize(n);
-    fill(dist.begin(),dist.end(),((1ull<<63) - 1ull));
+    fill(dist.begin(),dist.end(),-((1ull<<63)));
     dist[0] = 0;
-    cout << bellman_Ford(0)<<"\n";
+    cout << bellman_Ford(0,n-1)<<"\n";
 
     return 0;
 }

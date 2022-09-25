ade274742c09ba4fa0887f9bcba88a1048268443
penguin_72487 <agar.io6100@gmail.com>
2022-09-25 10:07:44 +0800

    IMPC Fall ct1

---
+++ cf/IMPCFall/UniversalSolution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    unordered_map<char,int> cc_Win;// r 0 p 1 s 2
+    cc_Win['R'] = 0;
+    cc_Win['P'] = 1;
+    cc_Win['S'] = 2;
+    string s;
+    while(t--){
+        cin >> s;
+
+    }
+    return 0;
+}

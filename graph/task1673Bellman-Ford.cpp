42f573aba904e58facf3b97280d37482e3bf673a
penguin72487 <agar.io6100@Gmail.com>
2022-09-30 22:02:22 +0800

    bellman_Ford use vector pass by

--- graph/task1673Bellman-Ford.cpp
+++ graph/task1673Bellman-Ford.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<set>
 #include<algorithm>
+#include <tuple>
 using namespace std;
 vector<long long> dist;
 vector<vector<pair<int, int>>> tunnel;
-queue<int> STL;
+queue<tuple<int, int, int>> STL;
 vector<unsigned long long> pass_By;
 bool flag=0;
 bool cycle=0;
 void bellman_Ford(int now)
 {
-   
-    vector<bool> pass_Node(dist.size(),0);
-    STL.push(now);
+    set<tuple<int, int, int>> path;
+    for (auto it = tunnel[now].begin(); it != tunnel[now].end();++it)
+    {
+        STL.push(make_tuple(now, it->first, it->second));
+    }
+
     //pass_Node[now] = 1;
     // for (auto it = tunnel[now].begin(); it != tunnel[now].end();++it)
     // {
@@ -22,23 +27,25 @@ void bellman_Ford(int now)
 
     while(!STL.empty())
     {
-        int t_Now=STL.front();
+        int t_Now, v, u;
+        tie(t_Now,v,u)=STL.front();
         STL.pop();
         ++pass_By[t_Now];
-        for (auto it = tunnel[t_Now].begin(); it != tunnel[t_Now].end();++it)
+        if(path.find(make_tuple(t_Now, v, u)) != path.end())
         {
-            if(pass_Node[it->first])
+            continue;
+        }
+        path.insert(make_tuple(t_Now, v, u));
+        if(dist[t_Now]+u>dist[v])
+        {
+            flag = 1;
+            dist[v] = dist[t_Now] + u;
+            for (auto it = tunnel[v].begin(); it != tunnel[v].end();++it)
             {
-                continue;
-            }
-            else if(dist[t_Now]+it->second>dist[it->first])
-            {
-                flag=1;
-                dist[it->first] = dist[t_Now] + it->second;
-                STL.push(it->first);
-                pass_Node[it->first] = 1;
+                STL.push(make_tuple(t_Now, it->first, it->second));
             }
         }
+        
     }
 }
 long long bellman_Ford(int now,int target)

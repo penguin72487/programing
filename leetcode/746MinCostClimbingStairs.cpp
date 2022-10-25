049de841e30c0dd0121e8f33ea3c2ae720f4d7a5
penguin72487 <agar.io6100@Gmail.com>
2022-10-25 21:26:06 +0800

    leetcode

--- leetcode/746MinCostClimbingStairs.cpp
+++ leetcode/746MinCostClimbingStairs.cpp
@@ -4,15 +4,31 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n,2147483647);
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        bool flag = false;
-        for (int i = 0; i < n;++i){
-
+        cost.push_back(0);
+        vector<int> dp(cost);
+        vector<int> from(n+1,-1);
+        //dp[0] = 0;
+        //dp[1] = 0;
+        ++n;
+        //bool flag = false;
+        for (int i = 2; i < n;++i){
+            // if(dp[i-2]<=dp[i-1])
+            // {
+            //     from[i] = i - 2;
+            // }
+            // else {
+            //     from[i] = i - 1;
+            // }
+            dp[i] = min(dp[i - 1], dp[i - 2])+cost[i];
         }
+        // cost.clear();
+        // int now = n - 1;
+        // for (int now = n - 1; from[now] != -1;now=from[now])
+        // {
+        //     cost.push_back(now);
+        // }
 
-            return 0;
+        return dp[n-1];
     }
 };
 int main()
@@ -20,6 +36,7 @@ int main()
     cout.tie(0)->sync_with_stdio(0);
     cin.tie(0);
     Solution s;
-    vector<int> cost = {10, 15, 20};
+    //vector<int> cost = {10,15,20};
+    vector<int> cost = {1,100,1,1,1,100,1,1,100,1};
     cout<<s.minCostClimbingStairs(cost);
 }

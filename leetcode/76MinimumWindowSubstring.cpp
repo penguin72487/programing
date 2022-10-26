688f0ad937eedafd745cd530217c41a5f82b7a3a
penguin_72487 <agar.io6100@gmail.com>
2022-10-26 20:04:46 +0800

    leetcode

--- leetcode/76MinimumWindowSubstring.cpp
+++ leetcode/76MinimumWindowSubstring.cpp
@@ -3,7 +3,34 @@ using namespace std;
 class Solution {
 public:
     string minWindow(string s, string t) {
-        
+        vector<int> a(26,0);
+        vector<int> b(26,0);
+        // for(auto it=s.begin(); it!=s.end(); ++it)
+        // {
+        //     ++a[*it - 'a'];
+        // }
+        unordered_map<char, bool> cb_Map;
+        for(auto it=t.begin(); it!=t.end(); ++it)
+        {
+            ++b[*it - 'a'];
+            cb_Map[*it] = true;
+        }
+        vector<string::iterator> sit;
+        for (auto it = s.begin();it!=s.end(); ++it)
+        {
+            if(cb_Map.find(*it) != cb_Map.end())
+            {
+                sit.push_back(it);
+            }
+        }
+        ++b[*sit[0] - 'a'];
+        string s="";
+        for(auto it = next(sit.begin());it!=sit.end(); ++it)
+        {
+            --[**prev(it) - 'a'];
+            ++b[**it - 'a'];
+        }
+        return s;
     }
 };
 int main()
---
+++ leetcode/templatestring.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string interpret(string command) {
+        string cash;
+        string ans;
+        for(auto it=command.begin(); it!=command.end();++it)
+        {
+            cash += *it;
+            if(cash=="G")
+            {
+                ans += cash;
+                cash.clear();
+            }
+            else if(cash=="()")
+            {
+                ans+="o";
+                cash.clear();
+            }
+            else if(cash=="(al)")
+            {
+                ans+="al";
+                cash.clear();
+            }
+            
+        }
+        return ans;
+    }
+};
+int main()
+{
+    cout.tie(0)->sync_with_stdio(0);
+    cin.tie(0);
+    Solution s;
+    cout<<s.interpret("G()(al)");
+}

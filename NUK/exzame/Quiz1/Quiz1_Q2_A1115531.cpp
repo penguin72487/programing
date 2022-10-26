fee19c686d576df58d4df87c95a643d3b4ef9df0
penguin72487 <agar.io6100@Gmail.com>
2022-10-26 20:07:54 +0800

    Q?

--- NUK/exzame/Quiz1/Quiz1_Q2_A1115531.cpp
+++ NUK/exzame/Quiz1/Quiz1_Q2_A1115531.cpp
@@ -1,29 +1,29 @@
-#include<bits/stdc++.h>
+#include <iostream> // cin cout
+#include <numeric> // accumulate -> sum of vector
+#include <vector> // similar to array but dynamic 
+#include <algorithm>// max_element min_element
+#include <iomanip> //fixed -> fixed the the decimal point if not whith  setprecision(n) output n digit (totle of before and after the decimal)    setprecision(3) -> the third digit after the decimal point 
 using namespace std;
 int main(){
-    
+    cout.tie(0)->sync_with_stdio(0); // input output aka io optimization
+    cin.tie(0);
     int n;
-    int i = 0;
-    int i_Max = 0;
-    int i_Min = 2147483647;
-    long long sum = 0;
-    cout << "please input a positive integer (0 end) : ";
-    while (cin >> n)
+    vector<int> vec;
+    
+
+    //cout << "please input a positive integer (0 end) : ";
+
+
+
+    while (cin >> n&&n!=0)
     {
-        if(n == 0)
-        {
-            break;
-        }
-        ++i;
-        i_Max = max(i_Max, n);
-        i_Min=min(i_Min, n);
-        sum += n;
-        cout << "\nplease input a positive integer (0 end) : ";
+        vec.push_back(n); // add element to vector
+        //cout << "\nplease input a positive integer (0 end) : ";
     }
-    //cout.tie(0)->sync_with_stdio(0);
-    cout << "max = " << i_Max<<"\n";
-    cout << "min = " << i_Min << "\n";
-    long double AVG = ((long double)sum / (long double)i)+(long double)0.0005;
+
+    cout << "max = " << *max_element(vec.begin(),vec.end())<<"\n";
+    cout << "min = " << *min_element(vec.begin(),vec.end()) << "\n";
+    long double AVG = ((float)(accumulate(vec.begin(),vec.end(),0)) / (float)vec.size())+0.0005f;
     cout << "AVG = " << fixed<<setprecision(3)<<AVG << "\n";
 
     return 0;

2f580fbeab327fe28d6f512009e34f6c24f4555a
penguin_72487 <agar.io6100@gmail.com>
2022-11-14 14:25:02 +0800

    bellman spaf

--- graph/task1197bellmanSPFA.cpp
+++ graph/task1197bellmanSPFA.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 #include<tuple>
 #include<unordered_map>
+#include<map>
+#include<queue>
 using namespace std;
 int main(){
     cin.tie(0)->sync_with_stdio(0);
@@ -12,36 +14,51 @@ int main(){
     vector<long long> path_From(n,-1);
     dist[0] = 0;
     vector<tuple<int, int, long long>> vec;
+    queue<tuple<int, int, long long>> q;
+    map<tuple<int, int, long long>, bool> pass;
+    map<tuple<int, int, long long>, int> count_Pass;
+    
     int a, b;
     long long c;
     for (int i = 0;i < m; ++i)
     {
         cin >> a >> b >> c;
         vec.push_back(make_tuple(a-1,b-1,c));
-
+        q.push(vec.back());
+        pass[vec.back()] = 1;
+        count_Pass[vec.back()] = 1;
     }
     
     bool flag = 0;
     int u, v;
     long long d;
     int op=0;
-    for (int i = 0; i < n; ++i)
-    {   
-        flag = 0;
-        for(auto it = vec.begin();it != vec.end();++it)
+    while(!q.empty())
+    {
+        if(count_Pass[q.front()]>n)
         {
-            tie(u, v, d) = *it;
-            if(dist[u]+d<dist[v])
-            {
-                flag = 1;
-                dist[v] = dist[u]+d;
-                path_From[v] = u;
-            }
-        }
-        if(!flag)
-        {
+            flag = 1;
             break;
         }
+        tie(u, v, d) = q.front();
+        pass[q.front()] = 0;
+        q.pop();
+
+        if(dist[u]+d<dist[v])
+        {
+            //flag = 1;
+            dist[v] = dist[u]+d;
+            path_From[v] = u;
+            op = v;
+            if(pass[make_tuple(u,v,d)]==0)
+            {
+                q.push(make_tuple(u, v, d));
+                ++count_Pass[vec.back()];
+                pass[make_tuple(u, v, d)]=1;
+            }
+            
+        }
+        
     }
     
     if(flag)

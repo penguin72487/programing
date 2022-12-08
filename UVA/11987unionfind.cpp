3554e8ae5727fb42e63818cb03852115172fe355
penguin_72487 <agar.io6100@gmail.com>
2022-12-08 17:23:18 +0800

    unfi

--- UVA/11987unionfind.cpp
+++ UVA/11987unionfind.cpp
@@ -1,10 +1,129 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class unifi{//union find
+    public:
+    int n;
+    vector<int> uni;
+    vector<set<int>> uni_Graph;
+    vector<long long> sum_Uni;
+    unifi(int t_n){
+        n = t_n;
+        uni.resize(n);
+        uni_Graph.resize(n);
+        sum_Uni.resize(n);
+        for(int i=0; i<n;++i)
+        {
+            uni[i] = i;
+            sum_Uni[i] = i;
+            uni_Graph[i].insert(i);
+        }
+    }
+    void a_To_setB(int a, int b){// move a to the set include b
+        if(find_F(a)==find_F(b))
+        {
+            return;
+        }
+        else {
+            
+            if(uni[a]==a)
+            {
+                if(uni_Graph[a].size()>1)// if father would move
+                {
+                    int num = *uni_Graph[a].begin();
+                    uni[num] = num;
+                    setA_To_setB(a, num);
+                    uni[a] = a;
+                    uni_Graph[num].erase(a);
+                    uni_Graph[a].insert(a);
+                    setA_To_setB(a, b);
+                }
+                else {
+                    setA_To_setB(a, b);
+                }
+            }
+            else {
+                uni_Graph[find_F(a)].erase(a);
+                sum_Uni[find_F(a)] -= a;
+
+                uni[a] = a;
+                sum_Uni[a] += a;
+                uni_Graph[a].insert(a);
+                setA_To_setB(a, b);
+            }
+            // sum_Uni[find_F(a)] -= a;
+            // sum_Uni[find_F(b)] += a;
+        }
+    }
+    void setA_To_setB(int a, int b){ // move set A into set B
+        if(find_F(a)==find_F(b))
+        {
+            return;
+        }
+        else {
+            
+            sum_Uni[find_F(b)] += sum_Uni[find_F(a)];
+            sum_Uni[find_F(a)] = 0;
+            int f = find_F(a);
+            for(auto& it:uni_Graph[find_F(a)])
+            {
+                uni_Graph[find_F(b)].insert(it);
+                uni[it] = find_F(b);
+            }
+            uni_Graph[f].clear();
+            uni[a] = find_F(b);
+        }
+    }
+    int find_F(int op)
+    {
+        if(uni[op]==op)
+        {
+            return op;
+        }
+        return find_F(uni[op]);
+    }
+
+};
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     cout.tie(0);
     int n, m;
-    cin >> n >> m;
+    while(cin >> n >> m)
+    {
+        if(n==m&&n==0)
+        {
+            break;
+        }
+        ++n;
+
+        unifi uni(n);
+        for (int i = 0; i < m;++i)
+        {
+            int type;
+            cin >> type;
+            if(type ==1)
+            {
+                int a, b;
+                cin >> a >> b;
+                uni.setA_To_setB(a,b);
+
+            }
+            else if(type ==2)// move a to the set include b
+            {
+                int a, b;
+                cin >> a >> b;
+                uni.a_To_setB(a, b);
+                //uni[a] = find_F(b, uni);
+
+            }
+            else if(type ==3)
+            {
+                int a;
+                cin >> a;
+                cout << uni.uni_Graph[uni.find_F(a)].size() << " " << uni.sum_Uni[uni.find_F(a)]<<"\n";
+            }
+        }
+    }
     
-    return 0;
+        return 0;
 }
--- UVA/tempCodeRunnerFile.cpp
+++ UVA/tempCodeRunnerFile.cpp
@@ -1,49 +1,83 @@
-
 #include<bits/stdc++.h>
 using namespace std;
+int find_F(int op,vector<int> &uni)
+{
+    if(uni[op]==op)
+    {
+        return op;
+    }
+    return find_F(uni[op], uni);
+}
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     cout.tie(0);
-    int n;
-    int k = 0;
-    while(cin>>n&&n!=0){
-        ++k;
-        string s;
-        vector<vector<int>> c(n, vector<int>(26, 0));
-        for(int i=0; i<n;++i)
-        {
-            cin >> s;
-            for(auto& it:s)
-            {
-                ++c[i][it - 'a'];
-            }
-        }
-        int ans = 0;
-        vector<int> vi(31);
-        for(int i=0; i<n;++i)
-        {
-            bool flag = 1;
-            for(auto& it:c[i])
-            {
-                ++vi[it];
-            }
-            for (int i = 1; i < 31;++i)
-            {
-                if(vi[i] >1)
-                {
-                    flag = 0;
-                    break;
-                }
-            }
-            ans += flag;
-        }
-
-
-
-        cout <<"Case "<<k<<": "<< ans << "\n";
+    int n, m;
+    cin >> n >> m;
+    ++n;
+    vector<int> uni(n);
+    vector<set<int>> uni_Graph(n);
+    vector<long long> sum_Uni(n);
+    for(int i=0; i<n;++i)
+    {
+        uni[i] = i;
+        sum_Uni[i] = i;
+        uni_Graph[i].insert(i);
     }
-    return 0;
+    for (int i = 0; i < m;++i)
+    {
+        int type;
+        cin >> type;
+        if(type ==1)
+        {
+            int a, b;
+            cin >> a >> b;
+            if(find_F(a,uni)==find_F(b,uni))
+            {
+                continue;
+            }
+            else {
+                
+                sum_Uni[find_F(b,uni)] += sum_Uni[find_F(a, uni)];
+                sum_Uni[find_F(a, uni)] = 0;
+                int f = find_F(a, uni);
+                for(auto& it:uni_Graph[find_F(a, uni)])
+                {
+                    uni_Graph[find_F(b, uni)].insert(it);
+                    uni[it] = find_F(b, uni);
+                }
+                uni_Graph[f].clear();
+                uni[a] = find_F(b, uni);
+            }
+
+        }
+        else if(type ==2)// move a to the set include b
+        {
+            int a, b;
+            cin >> a >> b;
+            if(find_F(a,uni)==find_F(b,uni))
+            {
+                continue;
+            }
+            else {
+                sum_Uni[find_F(a,uni)] -= a;
+                sum_Uni[find_F(b,uni)] += a;
+                if(uni[a]==a)
+                {
+                    uni_Graph[find_F(a, uni)].erase(a);
+                    uni[a] = find_F(b, uni);
+                }
+                uni_Graph[find_F(b, uni)].insert(a);
+                uni[a] = find_F(b, uni);
+            }
+            //uni[a] = find_F(b, uni);
+
+        }
+        else if(type ==3)
+        {
+            int a;
+            cin >> a;
+            cout << uni_Graph[find_F(a,uni)].size() << " " << sum_Uni[find_F(a,uni)]<<"\n";
+        }
+    }
+        return 0;
 }
-
-
-

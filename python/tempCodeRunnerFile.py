0e9d1fb8dd7b72d37f44f0011c76d9010e863b70
penguin72487 <agar.io6100@Gmail.com>
2023-01-10 20:17:45 +0800

    nuk final exame

---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+for i in range(q):
+#     quest =list(map(int,input().split()))
+#     for j in range(k):
+#         now[quest[j]] = last[j]
+#     last = now
+#     for j in range(q):
+#         print(now[j])
+#     print("\n")

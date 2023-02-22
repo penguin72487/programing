c6269777947aabe4774073c847d6b94d34e64011
penguin72487 <agar.io6100@Gmail.com>
2023-02-22 11:18:32 +0800

    toi

--- NUK/code/BePositive.cpp
+++ NUK/code/BePositive.cpp
@@ -6,27 +6,29 @@ int main()
     cin >> n;
     vector<int> a(n);
    for(auto& i:a)
-   {
-       cin >> i;
-   }
-   if(count_if(a.begin(), a.end(), [](int i) {
+    {
+        cin >> i;
+    }
+    if(count_if(a.begin(), a.end(), [](int i) {
        return i == 0;
-                                            })>a.size()/2.0)
-   {
+    })>a.size()/2.0)
+    {
        cout << 0;
-   }
-   else if (count_if(a.begin(), a.end(), [](int i)
-                     { return i > 0; })>=a.size()/2.0)
-                     {
-                            cout << 1;
-                     }
-    else if(count_if(a.begin(), a.end(), [](int i)
-                     { return i < 0; })>=a.size()/2.0)
-                     {
-                            cout << -1;
-                     }
-                     else
-                        {
-                                cout << 0;
-                        }
+    }
+    else if (count_if(a.begin(), a.end(), [](int i){
+         return i > 0; 
+    })>=a.size()/2.0)
+    {
+        cout << 1;
+    }
+    else if(count_if(a.begin(), a.end(), [](int i){
+        return i < 0; 
+    })>=a.size()/2.0)
+    {
+        cout << -1;
+    }
+    else
+    {
+            cout << 0;
+    }
 }

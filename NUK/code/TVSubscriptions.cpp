90500b7adf9138a9688535862890f75b5e886112
penguin72487 <agar.io6100@Gmail.com>
2023-04-25 20:13:51 +0800

    PDOA

---
+++ NUK/code/TVSubscriptions.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, k, d;
+        cin >> n >> k >> d;
+        int a[n];
+        map<int, int> v;
+        for(int i=0; i<n; i++)
+        {
+            cin >> a[i];
+            v[a[i]] = 0;
+        }
+    
+        for(int i=0; i<d&&i<n; i++)
+        {
+            v[a[i]]++;
+        }
+        long long ans = count_if(v.begin(),v.end(), [](pair<int, int> i) {
+            return i.second > 0;
+        });
+        long long min_Ans = ans;
+        for(int i=d; i<n; i++)
+        {
+            if(v[a[i-d]]==1)
+            {
+                ans--;
+            }
+            v[a[i-d]]--;
+
+            if(v[a[i]]==0)
+            {
+                ans++;
+            }
+            v[a[i]]++;
+            min_Ans = min(min_Ans, ans);
+        }
+        cout << min_Ans << "\n";
+    }
+    return 0;
+}
+/*
+1
+3 2 2
+2 2 1
+*/
--- UVA/tempCodeRunnerFile.cpp
+++ UVA/tempCodeRunnerFile.cpp
@@ -1,12 +1,2 @@
-bool operator>=(tuple<long long, long long, long long> a, tuple<long long, long long, long long> b)
-// {
-//     auto [l, h, w] = a;
-//     auto [L, W, H] = b;
-//     if(l>L&&w>W)
-//     {
-//         return 1;
-//     }
-//     else {
-//         return 0;
-//     }
-// }
+cin.tie(0)->sync_with_stdio(0);
+    cout.tie(0);

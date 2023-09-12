2f1acc6467e5b0c12e0b9c6a57ae2463a6411fba
penguin_72487 <agar.io6100@gmail.com>
2023-09-12 22:05:19 +0800

    codeforce

--- crypto/strategy/pick.cpp
+++ crypto/strategy/pick.cpp
@@ -1,43 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define e 2.71828182845904523536
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     cout.tie(0);
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w",stdout);
-
-    deque<int> sample;
-    int now_Price;
-    for (int i = 0; i < 22;++i)
+    int n;
+    //int cycle=7;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;++i)
     {
-        cin >> now_Price;
-        sample.push_back(now_Price);
+        cin>>a[i];
     }
-    deque<tuple<int, int, int>> order; // tickets
-    queue<tuple<int, int, int>> Short;
-    queue<tuple<int, int, int>> Long;
-    int time = 0;
-    while(cin>>now_Price&&now_Price)
+    //int ans=0;
+    for(auto it:a)
     {
-        ++time;
-        order.push_back({*max_element(sample.begin(), sample.end()), *min_element(sample.begin(), sample.end()), time});
-        sample.pop_front();
-        sample.push_back(now_Price);
-
-        if(time-get<2>(order.front())>60)
-        {
-            order.pop_front();
-        }
-        if(time-get<2>(Short.front())>60)
-        {
-            order.pop_front();
-        }
-        if(time-get<2>(Long.front())>60)
-        {
-            order.pop_front();
-        }
-        
+        cout<<it<<" ";
     }
+    cout<<"\n";
+
 
     fclose(stdin);
     fclose(stdout);

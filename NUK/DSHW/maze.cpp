e7aebc9d3e72ba5f185eb1bdb9e4e270e4f472bd
penguin_72487 <agar.io6100@gmail.com>
2024-04-14 11:04:52 +0800

    Refactor maze.cpp to improve readability and performance

--- NUK/DSHW/maze.cpp
+++ NUK/DSHW/maze.cpp
@@ -1,13 +1,12 @@
 #include<bits/stdc++.h>
 #define endl "\n"
 using namespace std;
-string output (char** maze, int n, int m)
-{
+int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+int dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+string output (char** maze, int n, int m) {
     string s;
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
-        {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             s += maze[i][j];
         }
         s += "\n";
@@ -20,38 +19,42 @@ int main() {
     cout.tie(0);
 
     #ifdef ENABLE_TIMING
-        using namespace std::chrono;
-        auto start = high_resolution_clock::now();
+    using namespace std::chrono;
+    auto start = high_resolution_clock::now();
     #endif
-    string s;
-    string cash;
-    getline(cin, s);
-    int m = s.length() / 2;
-    cash +=s+" ";
-    while(cin.peek() != '\n')
-    {
-        getline(cin, s);
-        cash += s+" ";
-    }
-    int n = cash.length() / m;
 
-    cout << cash<<endl;
-    char maze[n][m];
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
-        {
-            maze[i][j] = cash[i*m+j];
+    string s, cash;
+    getline(cin, s);
+    int m = s.length() / 2+1;
+    cash += s + " ";
+    while (cin.peek() != '\n' && getline(cin, s)) {
+        cash += s + " ";
+        cin.sync();
+    }
+    int n = cash.length() / m/2;
+
+    cout << cash << endl;
+    cash.erase(remove(cash.begin(), cash.end(), ' '), cash.end());
+
+    char** maze = new char*[n];
+    for (int i = 0; i < n; i++) {
+        maze[i] = new char[m];
+        for (int j = 0; j < m; j++) {
+            maze[i][j] = cash[i * m + j];
         }
     }
+
     cout << output(maze, n, m);
 
-
+    for (int i = 0; i < n; i++) { // Cleanup
+        delete[] maze[i];
+    }
+    delete[] maze;
 
     #ifdef ENABLE_TIMING
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<nanoseconds>(stop - start);
-        cout << "\nTotal time taken: " << double (duration.count())/1000000.0 << " ms.\n";
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    cout << "\nTotal time taken: " << double (duration.count()) / 1000000.0 << " ms.\n";
     #endif
 
     return 0;
